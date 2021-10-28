f5cb232ad1bc87861a4c8994502084062ca23e51
ZL Chen <iec100535@gmail.com>
2021-10-28 13:37:41 +0800

    Add ini, lib, log, suite, suite_ci folder.

--- lib/reboot.py
+++ lib/reboot.py
@@ -6,27 +6,27 @@ Created on 2021/10/27
 @title: Reboot script under the linux environment.
 '''
 
-from time import sleep
+from time import sleep, time
 from os import system
 import sys
 
 class reboot_machine(object):
-	def reboot(self, times):
+	def reboot(self, times, ip):
 		try:
 			for i in range(int(times)):
 				print('Reboot times: ' + str(i+1))
-				# sleep(20)
+				for wait_time in range(times):
+					print('Waitting....' + str(wait_time + 1))
 				print('The system is rebooting..')
-				# system('echo sudo reboot')
-				self.ssh()
+				self.ssh(ip)
 			return True
 		except:
 			return False
 
-	def ssh(self):
-		# a = system('ssh -t -l zl 192.168.8.102 ifconfig enp0s3 | grep \"inet \" | awk {\'print $2\'}')
-		system('ssh -t -l zl 192.168.8.102 \"sudo reboot\"')
+	def ssh(self, ip):
+		# system('ssh -t -l zl 192.168.8.102 ifconfig enp0s3 | grep \"inet \" | awk {\'print $2\'}')
+		system('ssh -t -l zl ' + ip + ' \"sudo reboot\"')
 
 if __name__ == "__main__":
 	r = reboot_machine()
-	r.reboot(1)
+	r.reboot(1, '172.20.10.5')
--- suite/reboot_linux.py
+++ suite/reboot_linux.py
@@ -6,11 +6,17 @@ Created on 2021/10/27
 @title: Reboot for linux by unittest.
 '''
 
+from os import system
 import sys
+from time import sleep
 sys.path.insert(0, '..\\lib')
 from reboot import reboot_machine
 from nose.tools import *
 from HTMLTestRunner import *
+from configparser import ConfigParser
+
+ini = ConfigParser()
+reboot_linux_ini = ini.read('.\..\\ini\\reboot_linux.ini')
 
 class reboot(unittest.TestCase):
 	@classmethod
@@ -23,22 +29,24 @@ class reboot(unittest.TestCase):
 
 	def test_case_01_reboot(self):
 		self = reboot_machine()
-		print(self.reboot(1))
-		if self.reboot(1) == False:
+		self = self.reboot(30, '172.20.10.13')
+		if self == False:
 			raise(Exception('Error'))
 
 	def test_case_02_multi_reboot(self):
 		self = reboot_machine()
-		print(self.reboot(5))
-		if self.reboot(5) == False:
+		self = self.reboot(30, '172.20.10.13')
+		if self == False:
 			raise(Exception('Error'))
 
 if __name__ == '__main__':
-	report_dir=r'reboot.html'
+	system('del reboot.html')
+	sleep(1)
+	report_dir = 'reboot.html'
 	re_open = open(report_dir, 'wb')
 	suite = unittest.TestLoader().loadTestsFromTestCase(reboot)
 	runner = HTMLTestRunner(
-						stream=re_open, \
-						title=u'Linux auto reboot by ZL.', \
-						description=u'Test Report')
+						stream = re_open, \
+						title = 'Linux auto reboot by ZL.', \
+						description = 'Test Report')
 	runner.run(suite)
