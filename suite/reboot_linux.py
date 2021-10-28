de6e320b16e041a9b7d1c61fec0c6d5ffb6b11b8
ZL Chen <iec100535@gmail.com>
2021-10-28 14:47:31 +0800

    reboot_linux.py

--- suite/reboot_linux.py
+++ suite/reboot_linux.py
@@ -6,17 +6,18 @@ Created on 2021/10/27
 @title: Reboot for linux by unittest.
 '''
 
-from os import system
 import sys
-from time import sleep
+import time
+import configparser
 sys.path.insert(0, '..\\lib')
+from os import system
+from time import sleep
 from reboot import reboot_machine
 from nose.tools import *
 from HTMLTestRunner import *
-from configparser import ConfigParser
 
-ini = ConfigParser()
-reboot_linux_ini = ini.read('.\..\\ini\\reboot_linux.ini')
+ini = configparser.ConfigParser()
+ini.read('.\..\\ini\\reboot_linux.ini')
 
 class take_screen_shot():
 	def __init__(self, func):
@@ -42,21 +43,20 @@ class reboot(unittest.TestCase):
 	@take_screen_shot
 	def test_case_01_reboot(self):
 		self = reboot_machine()
-		self = self.reboot(1, '172.20.10.5')
+		self = self.reboot(1, ini.get('setting', 'ip'))
 		if self == False:
 			raise(Exception('Error'))
 
 	@take_screen_shot
 	def test_case_02_multi_reboot(self):
 		self = reboot_machine()
-		self = self.reboot(1000, '172.20.10.5')
+		self = self.reboot(ini.get('setting', 'run_time'), ini.get('setting', 'ip'))
 		if self == False:
 			raise(Exception('Error'))
 
 if __name__ == '__main__':
-	system('del reboot.html')
-	sleep(1)
-	report_dir = 'reboot.html'
+	now = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
+	report_dir = 'reboot_' + now + '.html'
 	re_open = open(report_dir, 'wb')
 	suite = unittest.TestLoader().loadTestsFromTestCase(reboot)
 	runner = HTMLTestRunner(
