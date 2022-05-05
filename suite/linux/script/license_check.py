b92478e9c6e15c828465af676ad8e095b74bbf80
jackiejay888 <jackiejay888@gmail.com>
2022-05-06 00:00:41 +0800

    line_notify.py

--- suite/linux/script/license_check.py
+++ suite/linux/script/license_check.py
@@ -39,15 +39,15 @@ class bbu_license_check(object):
 			sleep(2)
 			self.element_get(By.CSS_SELECTOR, '#license').click()
 			sleep(2)
-			license_generatedate_name = self.element_get(By.XPATH, '//*[@id="licenseVue"]/form/div[1]/section/div[2]/div[4]/label').text
-			license_generatedate = self.element_get(By.XPATH, '//*[@id="licenseVue"]/form/div[1]/section/div[2]/div[4]/div/span').text
-			remain_time_name = self.element_get(By.XPATH, '//*[@id="firstContent"]/div[2]/div/div[2]/div[2]/table/thead/tr/th[4]/div').text
-			remain_time = self.element_get(By.XPATH, '//*[@id="firstContent"]/div[2]/div/div[2]/div[3]/table/tbody/tr/td[4]/div').text
-			self.line_notify.send_message('\nAttention, please.\nBaiCells\'s BBU information: \n'
-											+ license_generatedate_name + ': ' + license_generatedate + '\n'
-											+ remain_time_name + ': ' + remain_time)
-			print(license_generatedate_name + ': ' + license_generatedate)
-			print(remain_time_name + ': ' + remain_time)
+			self.license_generatedate_name = self.element_get(By.XPATH, '//*[@id="licenseVue"]/form/div[1]/section/div[2]/div[4]/label').text
+			self.license_generatedate = self.element_get(By.XPATH, '//*[@id="licenseVue"]/form/div[1]/section/div[2]/div[4]/div/span').text
+			self.remain_time_name = self.element_get(By.XPATH, '//*[@id="firstContent"]/div[2]/div/div[2]/div[2]/table/thead/tr/th[4]/div').text
+			self.remain_time = self.element_get(By.XPATH, '//*[@id="firstContent"]/div[2]/div/div[2]/div[3]/table/tbody/tr/td[4]/div').text
+			remain_time_int = int(self.remain_time)
+			if remain_time_int == 180 or remain_time_int == 90 or remain_time_int <= 30:
+				self.line_notification()
+			print(self.license_generatedate_name + ': ' + self.license_generatedate)
+			print(self.remain_time_name + ': ' + self.remain_time)
 			sleep(2)
 		except:
 			raise
@@ -58,6 +58,11 @@ class bbu_license_check(object):
 		return WebDriverWait(self.driver, 10).until(
 				EC.presence_of_element_located((name, element))
 			)
+
+	def line_notification(self):
+		self.line_notify.send_message('\nAttention, please.\nBaiCells\'s BBU information: \n'
+										+ self.license_generatedate_name + ': ' + self.license_generatedate + '\n'
+										+ self.remain_time_name + ': ' + self.remain_time)
 
 if __name__ == '__main__':
 	l = bbu_license_check()
