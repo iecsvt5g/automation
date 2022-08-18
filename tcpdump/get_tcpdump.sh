5da92070b567bd9ac86851b669e53f15b913045d
hahahank <chang330@gmail.com>
2022-08-18 11:09:46 +0800

    Update get_tcpdump.sh

--- tcpdump/get_tcpdump.sh
+++ tcpdump/get_tcpdump.sh
@@ -5,7 +5,7 @@ mkdir -p $dest
 file_name="tcpdump-$day.pcap"
 echo "> Start tcpdump :"$file_name
 date
-#sudo timeout 60 tcpdump -i any  -w $dest/$file_name
-sudo timeout 60 tcpdump -i any -nn sctp -w $dest/$file_name
+#sudo timeout 1800 tcpdump -i any  -w $dest/$file_name
+sudo timeout 1800 tcpdump -i any -nn sctp -w $dest/$file_name
 
 echo "> Done"
