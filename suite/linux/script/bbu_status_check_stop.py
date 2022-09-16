f8b13ce61832654d50b4879067c49912ddd96dd7
ZL Chen <iec100535@gmail.com>
2022-09-16 15:57:05 +0800

    bbu_status_check update

--- suite/linux/script/bbu_status_check_stop.py
+++ suite/linux/script/bbu_status_check_stop.py
@@ -6,7 +6,7 @@ Created on 2022/08/25
 @title: BBU status check.
 '''
 
-import sys, subprocess
+import sys, subprocess, pymysql, time
 sys.path.insert(0, '/root/automation/suite/linux/lib')
 # from gmail_notify import gmail_notify
 from line_notify import line_notify
@@ -31,7 +31,32 @@ class bbu_check(object):
 		l_n.send_message(message)
 		self.bbu_status = 0
 
+	def cloud_db(self, _time, bbu_status):
+		try:
+			sql = {
+					# 'host': '20.212.112.202',
+					# 'port': 3306,
+					# 'user': 'TAO',
+					# 'password': 'admin',
+					# 'db': 'svt'
+					'host': '172.32.3.153',
+					'port': 3306,
+					'user': 'svt',
+					'password': '1qaz@WSXiecsvt5g',
+					'db': 'svt'
+					}
+			conn = pymysql.connect(**sql)
+			cur = conn.cursor()
+			sql = """INSERT INTO {table}(time, ip, bbu_status) VALUES(%s, %s, %s)""".format(table='bbu_status')
+			# cur.execute(sql, (_time, '172.32.3.155', r_0))
+			cur.execute(sql, (_time, '172.32.3.155', str(bbu_status)))
+			conn.commit()
+		except Exception as e:
+			raise e
+
 if __name__ == '__main__':
 	bbu_status_check = bbu_check()
 	mydb = infulx_db("172.32.3.196", 8086, 'admin', 'admin', 'influx')
 	mydb.write(infulx_db.get_bbu_status(bbu_status_check.log()))
+	_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
+	bbu_status_check.cloud_db(_time, bbu_status_check.log())
