4a365ccb0acb0c339fc988239f01e31066da7162
ZL Chen <iec100535@gmail.com>
2022-11-02 16:15:11 +0800

    agent.sh

--- suite_ci/linux/script/agent.sh
+++ suite_ci/linux/script/agent.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 
+echo 3b959fd8e286abdbc17bd2905a526ab5b7779e1500093afcaaace90d44bb9e45 > secret-file
 curl -sO http://192.168.99.120:8080/jnlpJars/agent.jar
-java -jar agent.jar -jnlpUrl http://192.168.99.120:8080/computer/svt/jenkins-agent.jnlp -secret a1a1b586b851416a411ca2be56081e6b25b832df9f42ea10d4e29757a6708082 -workDir "/root"
+java -jar agent.jar -jnlpUrl http://192.168.99.120:8080/computer/svt/jenkins-agent.jnlp -secret @secret-file -workDir "/root" &
