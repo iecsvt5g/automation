05fb76d7e138abce0ada8ebb9d41211722af89e7
ZL Chen <iec100535@gmail.com>
2023-04-27 17:56:21 +0800

    modified:   line_notify.py, modified:   ru_acc_parser.py

--- suite/linux/script/line_notify.py
+++ suite/linux/script/line_notify.py
@@ -14,11 +14,10 @@ class line_notify(object):
 		# headers = {
 		# 	'Authorization': 'Bearer ' + 'qJQeg65a8eQBfnXwZbZUb6539PN1c3cXrFvfgxxkXz7', # SVT Group Notification
 		# 	'Content-Type': 'application/x-www-form-urlencoded'
-		# }	
+		# }
 		headers = {
 			'Authorization': 'Bearer ' + 'tp9rUIAEvDHsELD9oDAeW3SPtO57bGWEqSpmJ7qCP1H', # Myself Group Notification
 			'Content-Type': 'application/x-www-form-urlencoded'
 		}
 		params = {'message': message}
-		r = requests.post('https://notify-api.line.me/api/notify', headers=headers, params=params)
-		print('Status code number:', r.status_code)  #200
+		requests.post('https://notify-api.line.me/api/notify', headers=headers, params=params)
