42ba809400100b0625a31cdbc4134384a265ae1f
ZL Chen <iec100535@gmail.com>
2023-05-24 10:47:54 +0800

    Clear the all data.

--- suite/linux/lib/create_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/
-tail Phy.log > /root/bler_test.txt
-exit
--- suite/linux/script/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con=mysqli_connect=("20.212.112.202","TAO","admin","test")
-if($con){
-    echo "DB success connect"
-}
-?>
