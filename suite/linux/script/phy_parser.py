cb6e90ad858b8a72331cd991ae6acad2736c8b43
Kent <iecsvt5g@gmail.com>
2023-07-31 13:56:23 +0800

    kent:phy_parser.py change to influxdb

--- suite/linux/script/phy_parser.py
+++ suite/linux/script/phy_parser.py
@@ -11,6 +11,8 @@ from pymysql import connect
 from time import *
 from configparser import ConfigParser
 from math import log
+from influxdb import InfluxDBClient
+
 
 config = ConfigParser()
 config.read('/etc/inventec_svt_deployment/setting.ini')
@@ -48,6 +50,8 @@ class phy(object):
 			time = time_parser[1].replace(',runningTime:', '')	# replace time
 			datetime = date + ' ' + time
 			datetime = self.datetime_taiwan_to_utc(datetime)	# UTC+8 to UTC
+			influxdbtime=strptime(datetime,'%Y-%m-%d %H:%M:%S')
+			influxdbtime=int((mktime(influxdbtime)+60*60*8)*1000)
 			time_list.append(datetime)
 			time_list = str(time_list[0])	# date and time
 		except:
@@ -96,7 +100,7 @@ class phy(object):
 				else:
 					pass
 			cell = str(cell)
-			self.insert_database(time_list, self._ip_parser(), host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr)
+			self.insert_database(influxdbtime, self._ip_parser(), host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr)
 
 	'''
 	IP Address Parser
@@ -115,26 +119,54 @@ class phy(object):
 	'''
 	Insert into date to MySQL (phpmyadmin)
 	'''
-	def insert_database(self, time_list, ip, host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr):
-		try:
-			mysql_info = {
-				# 'host': '172.32.3.153',
-				'host': config.get('setting', 'mysql_ip'),
-				'port': 3306,
-				'user': 'svt',
-				'password': '1qaz@WSXiecsvt5g',
-				'db': 'svt'
-			}
-			conn = connect(**mysql_info)
-			cur = conn.cursor()
-			sql = """INSERT INTO {table}(DateTime , IP, HOST_NAME, CELL, DL_Tput, UL_Tput_1, UL_Tput_2, UL_BLER, SRS_SNR) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)""".format(table='phy')
-			cur.execute(sql, (time_list, ip, host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr))
-			conn.commit()
-			print('The information is commit to database.')
-		except Exception as e:
-			# raise e
-			pass
+	# def insert_database(self, time_list, ip, host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr):
+	# 	try:
+	# 		mysql_info = {
+	# 			# 'host': '172.32.3.153',
+	# 			'host': config.get('setting', 'mysql_ip'),
+	# 			'port': 3306,
+	# 			'user': 'svt',
+	# 			'password': '1qaz@WSXiecsvt5g',
+	# 			'db': 'svt'
+	# 		}
+	# 		conn = connect(**mysql_info)
+	# 		cur = conn.cursor()
+	# 		sql = """INSERT INTO {table}(DateTime , IP, HOST_NAME, CELL, DL_Tput, UL_Tput_1, UL_Tput_2, UL_BLER, SRS_SNR) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)""".format(table='phy')
+	# 		cur.execute(sql, (time_list, ip, host_name, cell, dl_tput, ul_tput_1, ul_tput_2, ul_bler, srs_snr))
+	# 		conn.commit()
+	# 		print('The information is commit to database.')
+	# 	except Exception as e:
+	# 		# raise e
+	# 		pass
+ 
+	'''
+ 	insert into SMO influxdb
+  	'''
+	def insert_database(self,TIME , IP, HOST_NAME, CELL, DL_Tput, UL_Tput_1, UL_Tput_2, UL_BLER, SRS_SNR) :
+		try :
+			d= [{
+			"measurement": "phy_parser",
+			"tags": {
+				"ip": IP,
+				"hostname":HOST_NAME,
+				"cell" : CELL
+			},
+			"time": TIME,
+			"fields": { 'DL_1': DL_Tput, 
+						'UL_1': UL_Tput_1,
+						'UL_2': UL_Tput_2,
+						'BLER': UL_BLER,
+						'SRS_SNR': SRS_SNR
+					}
+				}]
 
+			client = InfluxDBClient("172.32.3.68",8086,'admin','admin','svt')
+			client.write_points(d)
+			print('Influxdb Insert Data GOOOOOOD')
+		except :
+			print('Influxdb Insert Data BAAAAAAD')
+ 
+ 
 if __name__ == '__main__':
 	while True:
 		func = phy()
